a64cb86bef282048550b6af00c916ff0af1631f1
Burak Öztaş <143541613+BurakOztas94@users.noreply.github.com>
2023-09-25 19:26:41 +0300

    Pattern Related Method

--- PatternRelatedMethod/src/Main.java
+++ PatternRelatedMethod/src/Main.java
@@ -1,36 +1,11 @@
 import java.util.Scanner;
-
 public class Main {
-
-    static boolean patternCalculator ( int a )
-        {
-
-
-            if ( a > 0 )
-                {
-                    System.out.print ( a + " "  );
-                    a -= 5;
-
-
-                    if ( a == 0 ||a<0)
-                        {
-                            System.out.print ( a + " " );
-                            a += 5;
-
-                        }
-
-
-
-                }
-            return patternCalculator ( a );
-        }
-
     public static void main ( String[] args )
         {
             Scanner scan = new Scanner ( System.in );
             System.out.print ( "Please enter number : " );
             int number = scan.nextInt ();
-
-            patternCalculator ( number );
+            PatternMethod pattern = new PatternMethod ();
+            pattern.patternCalculator ( number , number , true );
         }
 }
