8ecd3020f2b308aec87b41561f31c19853ad78a8
Burak Öztaş <143541613+BurakOztas94@users.noreply.github.com>
2023-10-02 22:45:04 +0300

    Repetetive Even Number

--- BwritingWithStarsUsingArrays/src/Main.java
+++ BwritingWithStarsUsingArrays/src/Main.java
@@ -7,13 +7,13 @@ public class Main {
                 {
                     for ( int j = 0;j < letter[i].length;j++ )
                         {
-                            if ( i == 0 || i == 3 || i == 6 )
+                            if ( i == 0 || i == 6 )
                                 {
                                     letter[i][j] = " * ";
                                 }
                             else if ( j == 0  )
                                 {
-                                    letter[i][j] = " *       *  ";
+                                    letter[i][j] = " *        *   ";
                                 }
                             else
                                 {
